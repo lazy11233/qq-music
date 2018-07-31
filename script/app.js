086e5ec17d7ea92b131ee841a8c2cae607b473d3
lazy11233 <1257625996@qq.com>
2018-07-31 16:40:23 +0800

    merge

---
+++ script/app.js
@@ -0,0 +1,10 @@
+    let slider = new Slider({
+        el: document.querySelector("#slider"),
+        slides: [
+            {link: "#1",image: "images/31001.jpg"},
+            {link: "#2",image: "images/31002.jpg"},
+            {link: "#3",image: "images/31003.jpg"},
+            {link: "#4",image: "images/31004.jpg"},
+            {link: "#5",image: "images/31005.jpg"}
+        ]
+    });
